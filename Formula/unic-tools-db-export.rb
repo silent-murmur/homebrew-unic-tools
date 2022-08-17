afc052f7fa386655f2d62d23cad11d6ae29b1966
Roman Domke <roman.domke@unic.com>
2022-08-17 13:09:45 +0200

    Update UnicToolsDbExport formula

--- Formula/unic-tools-db-export.rb
+++ Formula/unic-tools-db-export.rb
@@ -1,7 +1,7 @@
 class UnicToolsDbExport < Formula
   desc "Exports a database from a drupal pod in a kubernetes cluster"
   homepage "https://github.com/silent-murmur/unic-tools-db-export"
-  url "https://github.com/silent-murmur/unic-tools-db-export/archive/refs/tags/v1.0.0.tar.gz"
+  url "https://github.com/silent-murmur/unic-tools-db-export/archive/refs/tags/v1.0.1.tar.gz"
   sha256 "ee1fcf791287ed98ba9e979df7ad8a279bd63d5e466c77a59229974738a6b399"
   license "MIT"
 
